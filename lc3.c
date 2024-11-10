a5998173c554721490eb6692abe4e2228adbd25a
Kumar Saurav <krsaurav2001@gmail.com>
2024-11-10 18:01:34 +0530

    Create memory

---
+++ lc3.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+//Constructing the memory of the computer 
+// The LC3 computer has 2^16 = 65,536 memory locations each 
+// storing a 16-bit value 
+
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX]; 
+
+int main() {
+    return 0;
+}
