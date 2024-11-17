b25078cb0ac51d09eb60e6edf73d197aafa68106
Kumar Saurav <krsaurav2001@gmail.com>
2024-11-17 22:20:37 +0530

    Add image file reading functions.

--- lc3.c
+++ lc3.c
@@ -250,6 +250,41 @@ void op_not(uint16_t instruction) {
     update_flags(destination_register);
 }
 
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
+}
+
+void read_image_file(FILE* file) {
+    /*
+        Each LC3 program starts with 16 bits specifying the 
+        memory address where the program should start.
+    */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t *p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while(read-- > 0) {
+        *p = swap16(*p);
+        ++p;
+    }
+
+}
+
+int read_image(const char* image_path) {
+    FILE* file = fopen(image_path, "rb");
+    if(!file) {
+        return 0;
+    }
+    read_image_file(file);
+    fclose(file);
+    return 1;
+
+}
+
 int main(int argc, const char* argv[]) {
     // If the number of arguments provided is not sufficient,
     // we print a user guide on how to use it
