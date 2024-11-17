af77cb609b8a76fbbb672971c9eebdcc32fdfac0
Kumar Saurav <krsaurav2001@gmail.com>
2024-11-17 23:09:04 +0530

    Rearrange code.

--- lc3.c
+++ lc3.c
@@ -5,6 +5,29 @@
 #include <Windows.h>
 #include <conio.h>  // _kbhit
 
+
+enum { // Memory mapped registers
+    
+    // Special registers not accesible from the normal 
+    // register table, instead stored at a specific location in 
+    // memory, commonly used to interact with special hardware 
+    // devices. 
+    
+    MR_KBSR = 0xFE00,  // Keyboard Status
+    MR_KBDR = 0xFE02   // Keyboard Data 
+};
+
+enum { // Trap codes 
+    TRAP_GETC = 0x20,  // Get a character from the keyboard, not echoed to the terminal 
+    TRAP_OUT = 0x21,   // Output a character 
+    TRAP_PUTS = 0x22,  // Output a word string 
+    TRAP_IN = 0x23,    // Get a character from the keyboard, echoed to the terminal 
+    TRAP_PUTSP = 0x24, // Output a byte string
+    TRAP_HALT = 0x25   // Halt the program 
+};
+
+
+
 //Constructing the memory of the computer 
 // The LC3 computer has 2^16 = 65,536 memory locations each 
 // storing a 16-bit value 
@@ -33,17 +56,14 @@ enum { // Registers
 };
 uint16_t registers[R_COUNT];
 
-enum { // Memory mapped registers
-    
-    // Special registers not accesible from the normal 
-    // register table, instead stored at a specific location in 
-    // memory, commonly used to interact with special hardware 
-    // devices. 
-    
-    MR_KBSR = 0xFE00,  // Keyboard Status
-    MR_KBDR = 0xFE02   // Keyboard Data 
+enum { // Condition Flags
+    FL_POS = 1 << 0,
+    FL_ZRO = 1 << 1,
+    FL_NEG = 1 << 2
 };
 
+//Flag to describe whether the program is running or not 
+int running = 1;
 
 HANDLE hStdin = INVALID_HANDLE_VALUE;
 DWORD fdwMode, fdwOldMode;
@@ -63,19 +83,70 @@ void restore_input_buffering(){
     SetConsoleMode(hStdin, fdwOldMode);
 }
 
+uint16_t check_key(){
+    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
+}
+
 void handle_interrupt(int signal) {
     restore_input_buffering();
     printf("\n");
     exit(-2);
 }
 
+uint16_t sign_extend(uint16_t x, int bit_count) {
+    if(x >> (bit_count - 1) & 1) {
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+uint16_t swap16(uint16_t x) {
+    return (x << 8) | (x >> 8);
+}
+
+void update_flags(uint16_t r) {
+    if(registers[r] == 0) {
+        registers[R_COND] = FL_ZRO;
+    }
+    else {
+        registers[R_COND] = (registers[r] >> 15 ? FL_NEG : FL_POS);
+    } 
+}
+
+void read_image_file(FILE* file) {
+    /*
+        Each LC3 program starts with 16 bits specifying the 
+        memory address where the program should start.
+    */
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t max_read = MEMORY_MAX - origin;
+    uint16_t *p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    while(read-- > 0) {
+        *p = swap16(*p);
+        ++p;
+    }
+
+}
+
+int read_image(const char* image_path) {
+    FILE* file = fopen(image_path, "rb");
+    if(!file) {
+        return 0;
+    }
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
 void memory_write(uint16_t address, uint16_t value) {
     memory[address] = value;
 }
 
-uint16_t check_key(){
-    return WaitForSingleObject(hStdin, 1000) == WAIT_OBJECT_0 && _kbhit();
-}
 uint16_t memory_read(uint16_t address) {
     if(address != MR_KBSR) {
         return memory[address];
@@ -117,27 +188,6 @@ enum { // Instruction set
 // R_COND register to check logical conditions on the last performed 
 // operation 
 
-enum { // Condition Flags
-    FL_POS = 1 << 0,
-    FL_ZRO = 1 << 1,
-    FL_NEG = 1 << 2
-};
-
-uint16_t sign_extend(uint16_t x, int bit_count) {
-    if(x >> (bit_count - 1) & 1) {
-        x |= (0xFFFF << bit_count);
-    }
-    return x;
-}
-
-void update_flags(uint16_t r) {
-    if(registers[r] == 0) {
-        registers[R_COND] = FL_ZRO;
-    }
-    else {
-        registers[R_COND] = (registers[r] >> 15 ? FL_NEG : FL_POS);
-    } 
-}
 
 void op_add(uint16_t instruction) {
     uint16_t destination_register = (instruction >> 9) & 0x7;
@@ -251,17 +301,8 @@ void op_store_base_plus_offset(uint16_t instruction) {
     memory_write(registers[base_register] + offset, registers[source_register]);
 }
 
-enum { // Trap codes 
-    TRAP_GETC = 0x20,  // Get a character from the keyboard, not echoed to the terminal 
-    TRAP_OUT = 0x21,   // Output a character 
-    TRAP_PUTS = 0x22,  // Output a word string 
-    TRAP_IN = 0x23,    // Get a character from the keyboard, echoed to the terminal 
-    TRAP_PUTSP = 0x24, // Output a byte string
-    TRAP_HALT = 0x25   // Halt the program 
-};
 
 void op_system_call(uint16_t instruction) {
-    uint16_t trap_vector = instruction & 0xFF; //Implicitly zero extended here 
     registers[R_R7] = registers[R_PC];
     // Execute system calls, mostly for dealing with I/O
     switch (instruction & 0xFF) {
@@ -283,25 +324,29 @@ void op_system_call(uint16_t instruction) {
             break;
         case TRAP_IN:
             printf("Enter a character: ");
-            char c = getchar();
-            putc(c, stdout);
-            registers[R_R0] = (uint16_t) c;
+            char d = getchar();
+            putc(d, stdout);
+            fflush(stdout);
+            registers[R_R0] = (uint16_t) d;
             update_flags(R_R0);
             break;
         case TRAP_PUTSP:
-            uint16_t* c = memory + registers[R_R0];
-            while(*c) {
-                char char1 = (*c) & 0xFF;
+            uint16_t* e = memory + registers[R_R0];
+            while(*e) {
+                char char1 = (*e) & 0xFF;
                 putc(char1, stdout);
-                char char2 = (*c) >> 8;
+                char char2 = (*e) >> 8;
                 if (char2) putc(char2, stdout);
-                ++c;
+                ++e;
             }
             fflush(stdout);
             break;
         case TRAP_HALT:
             puts("HALT");
             fflush(stdout);
+            running = 0;
+            break;
+        default:
             break;
     }
 }
@@ -311,40 +356,6 @@ void op_not(uint16_t instruction) {
     uint16_t source_register = (instruction >> 6) & 0x7;
     registers[destination_register] = ~registers[source_register];
     update_flags(destination_register);
-}
-
-uint16_t swap16(uint16_t x) {
-    return (x << 8) | (x >> 8);
-}
-
-void read_image_file(FILE* file) {
-    /*
-        Each LC3 program starts with 16 bits specifying the 
-        memory address where the program should start.
-    */
-    uint16_t origin;
-    fread(&origin, sizeof(origin), 1, file);
-    origin = swap16(origin);
-
-    uint16_t max_read = MEMORY_MAX - origin;
-    uint16_t *p = memory + origin;
-    size_t read = fread(p, sizeof(uint16_t), max_read, file);
-
-    while(read-- > 0) {
-        *p = swap16(*p);
-        ++p;
-    }
-
-}
-
-int read_image(const char* image_path) {
-    FILE* file = fopen(image_path, "rb");
-    if(!file) {
-        return 0;
-    }
-    read_image_file(file);
-    fclose(file);
-    return 1;
 }
 
 int main(int argc, const char* argv[]) {
@@ -371,7 +382,6 @@ int main(int argc, const char* argv[]) {
     enum { PC_START = 0x3000};
     registers[R_PC] = PC_START;
 
-    int running = 1;
     while(running) {
         uint16_t instruction = memory_read(registers[R_PC]++);
         uint16_t op = instruction >> 12;
